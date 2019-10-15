aab547e5f02e561d518f8a4ac8188381072eacd2
Ates Yanik <albiberon@users.noreply.github.com>
2019-10-15 16:02:22 +0200

    Update main_logic.js

--- main_logic.js
+++ main_logic.js
@@ -1,51 +1,102 @@
+//https://geoip-db.com/json/
 
+var Websites = {
+
+    "INT" :   "https://www.junglegym.com",
+    "NL"  :   "https://www.junglegym.nl",
+    "DK"  :   "https://www.junglegym.dk",
+    "EE"  :   "https://www.junglegym.ee",
+    "ES"  :   "https://www.junglegym.es",
+    "FR"  :   "https://www.junglegym.fr",
+    "GR"  :   "https://www.junglegym.gr",
+    "IS"  :   "https://www.junglegym.is",
+    "MK"  :   "https://www.junglegym.mk",
+    "AT"  :   "https://www.junglegym.at",
+    "BE"  :   "https://www.junglegym.be",
+    "DE"  :   "https://www.junglegym.de",
+    "IT"  :   "https://www.junglegym.it",
+    "IE"  :   "https://www.junglegym.ie",
+    "LV"  :   "https://www.junglegym.lv",
+    "LU"  :   "https://www.junglegym.lu",
+    "PT"  :   "https://www.junglegym.pt",
+    "CH"  :   "https://www.junglegym.ch",
+    "SI"  :   "https://www.junglegym.si",
+    "ZA"  :   "https://www.junglegym.co.za",
+    "NO"  :   "https://www.junglegym.co.no",
+    "FI"  :   "https://www.junglegym.fi",
+    "SE"  :   "https://www.junglegym.se",
+    "PL"  :   "https://www.junglegym.pl",
+    "UA"  :   "https://www.junglegym.ua",
+    "UK"  :   "https://www.junglegym.co.uk"
+
+    };
+
+
+
+    var landedSite = window.location.origin;
+    var landedSiteCountry = landedSite.slice(-2).toUpperCase();
+
+    if( landedSiteCountry === 'UK'){
+        landedSiteCountry = 'GB';
+    }
+    
+    var visitorCountry;
+    var visitorCity;
+
+
+
+ 
 require(['jquery', 'domReady!'], function ($) {
 
 
-    var userVisitedAddress = window.location.origin;
-
-    var Websites = {
-        "Interinational": "https://www.junglegym.com",
-        "NL": "https://www.junglegym.nl",
-        "EE": "https://www.junglegym.ee",
-        "AT": "https://www.junglegym.at",
-        "BE": "https://www.junglegym.be",
-        "DE": "https://www.junglegym.de",
-        "IT": "https://www.junglegym.it",
-        "IE": "https://www.junglegym.ie",
-        "LV": "https://www.junglegym.lv",
-        "LU": "https://www.junglegym.lu",
-        "PT": "https://www.junglegym.pt",
-        
-
-        };
 
 
-        $.get("https://ipinfo.io", function(response) {
-          
-        
-        if (Websites.hasOwnProperty(response.country)){
-            console.log(
-                `
-                Howdy ${response.city}!
-                You are on a wrong page.
-                The right address is:
-                ${Websites[response.country]}
-                `
-            );
-        } else {
-            `
-            Howdy ${response.city}!
-            You better visit 
-            ${Websites['International']}
-            `
-        }
+		console.log(`Landed site is ${landedSite}
+					 Language of the website is ${landedSiteCountry}`);
 
-        }, "jsonp");
-    
-    // learn generating pop up dialog
-    //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal2
-    // https://www.sitepoint.com/14-jquery-modal-dialog-boxes/
-        
+        $.ajax({
+            url: "https://geoip-db.com/jsonp",
+            jsonpCallback: "callback",
+            dataType: "jsonp",
+            success: function( location ) {
+
+                visitorCountry = location.country_code;
+
+
+
+
+                visitorCity = location.city;
+				
+				console.log(`Visitor's country is ${visitorCountry}
+							 The city of the visitor is ${visitorCity}`)
+                if ( landedSiteCountry !== visitorCountry){
+                        var linkToShow = '';
+                    if ( Websites.hasOwnProperty(visitorCountry)){
+
+                        linkToShow = Websites[visitorCountry];
+
+                        console.log(
+                            `
+                            Howdy ${visitorCity}!
+                            You are on a wrong page.
+                            The right address is:
+                            ${linkToShow}
+                            `
+                        );
+                        
+                    } else {
+                        linkToShow = Websites.INT;
+                        console.log(
+                                    ` Howdy ${visitorCity}!
+                                      You better visit this site:
+                                      ${linkToShow} `
+                                      );
+                      } 
+
+                }  
+
+            }
+        });	
+
 
 });
